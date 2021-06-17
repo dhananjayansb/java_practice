54c08bb30f239cde74e957036b87e70f1d0bce39
dhananjayansb <dhananjayansb@gmail.com>
2021-06-17 11:43:34 +0530

    new code

---
+++ Challenging/Subset.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Subset
+{
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter number : ");
+		int number =s.nextInt();
+		int size,i,sum;
+		size = Arrays.toString(number).length();
+		System.out.print(size);
+		int[] array = new int[size];
+	}
+}
